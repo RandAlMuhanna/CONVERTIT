7b6bac05715e3286dee8f55e2278fb15f5e72370
RandAlMuhanna <Rand.y.almuhanna@gmail.com>
2023-01-05 11:22:58 +0300

    Initial Commit

---
+++ CONVERTIT/CONVERTITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CONVERTITApp.swift
+//  CONVERTIT
+//
+//  Created by Rand AlMuhanna on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CONVERTITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
